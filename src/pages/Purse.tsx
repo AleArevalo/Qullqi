5f12c3d186d171f41bf7575c2da67c1ac9c68133
Alexander Arévalo <alexander_arevalo@icloud.com>
2024-09-02 23:28:50 -0400

    Add localStorage integration in Purse to persist expenses and income, and enhance layout with improved heading and spacing

--- src/pages/Purse.tsx
+++ src/pages/Purse.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import { Movement } from '../interfaces/movement'
 import Summary from '../components/Summary'
@@ -9,8 +9,24 @@ const Purse = () => {
     const [ expenses, setExpenses ] = useState<Movement[]>([])
     const [ incomes, setIncome ] = useState<Movement[]>([])
 
+    useEffect(() => {
+        const dataExpenses = localStorage.getItem('expenses')
+        const dataIncome = localStorage.getItem('income')
+
+        if (dataExpenses) {
+            setExpenses(JSON.parse(dataExpenses))
+        }
+
+        if (dataIncome) {
+            setIncome(JSON.parse(dataIncome))
+        }
+    }, [])
+
     return (
-        <main className="w-3/4 mx-auto">
+        <main className="w-3/4 mx-auto my-8">
+            <h1 className="text-3xl font-bold text-center text-slate-900 dark:text-white">
+                Qullqi
+            </h1>
             <Summary />
             <Income values={ incomes } />
             <Expenses values={ expenses } />
