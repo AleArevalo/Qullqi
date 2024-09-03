709bf7eaa57cdf65b551ee9665789335c51a162b
Alexander Arévalo <alexander_arevalo@icloud.com>
2024-09-02 22:52:57 -0400

    Add "Aplicar filtro" button to Table component for enhanced user interaction and improved filter functionality

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -2,9 +2,12 @@ const Table = () => {
     return (
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
             <div className="flex items-center justify-between flex-column md:flex-row flex-wrap space-y-4 md:space-y-0 p-4 bg-white dark:bg-gray-900">
-                <div>
+                <div className="flex items-center space-x-4">
                     <button className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
                         Agregar
+                    </button>
+                    <button className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
+                        Aplicar filtro
                     </button>
                 </div>
                 <label htmlFor="table-search" className="sr-only">Search</label>
