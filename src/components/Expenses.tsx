4fe84bc8ca811e567995500281745c07fff4c0ab
Alexander Arévalo <alexander_arevalo@icloud.com>
2024-09-05 22:40:36 -0400

    Refactor Expenses component by simplifying layout and removing unnecessary search/filter controls for cleaner design

--- src/components/Expenses.tsx
+++ src/components/Expenses.tsx
@@ -3,25 +3,7 @@ import Table from "./Table"
 
 const Expenses = (props: Props) => {
     return (
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-4">
-            <div className="flex items-center justify-between flex-column md:flex-row flex-wrap space-y-4 md:space-y-0 p-4 bg-white dark:bg-gray-900">
-                <div className="flex items-center space-x-4">
-                    <button className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
-                        Aplicar filtro
-                    </button>
-                </div>
-                <label htmlFor="table-search" className="sr-only">Search</label>
-                <div className="relative">
-                    <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
-                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                        </svg>
-                    </div>
-                    <input type="text" id="table-search-users" className="block py-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Filtrar por texto" />
-                </div>
-            </div>
-            <Table values={ props.values } setValues={ props.setValues } />
-        </div>
+        <Table values={ props.values } setValues={ props.setValues } />
     )
 }
 
