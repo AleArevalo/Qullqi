d8bf51e006353b1595b3deddaaeccba0b7cd3d96
Alexander Arévalo <alexander_arevalo@icloud.com>
2024-09-13 20:27:42 -0300

    Add login/logout feature in Header component; include cloud icons for UI, with placeholder functions for handling clicks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,30 @@
+import { IconCloud, IconCloudX } from "@tabler/icons-react"
+
 const Header = () => {
+    const handleLogin = () => {
+        // TODO: handle click cloud
+    }
+
+    const handleLogout = () => {
+        // TODO: handle click cloud x
+    }
+
     return (
-        <h1 className="text-3xl font-bold text-center text-slate-900 dark:text-white my-6">
-            Qullqi
-        </h1>
+        <div className="grid grid-cols-3">
+            <h1 className="col-start-2 text-3xl font-bold text-center text-slate-900 dark:text-white my-6">
+                Qullqi
+            </h1>
+            <div className="col-start-3 content-center px-4">
+                <div className="flex gap-4 justify-end">
+                    <button className="text-black dark:text-white" onClick={ handleLogin }>
+                        <IconCloud />
+                    </button>
+                    <button className="text-black dark:text-white" onClick={ handleLogout }>
+                        <IconCloudX />
+                    </button>
+                </div>
+            </div>
+        </div>
     )
 }
 
