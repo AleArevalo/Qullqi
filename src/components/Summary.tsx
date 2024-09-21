03c47cd3e3da8f00d2ffdc24350dd29ea1d1d2de
Alexander Arévalo <alexander_arevalo@icloud.com>
2024-09-21 17:16:08 -0300

    Update Summary component: Enhance layout with responsive design changes and new elements for better user experience on mobile devices

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -6,9 +6,20 @@ import { PropsSummary } from '../interfaces/props'
 const Summary = (props: PropsSummary) => {
     return (
         <div className="top-0 left-0 w-full py-4">
-            <div className="grid grid-cols-1 sm:grid-cols-5 gap-4">
+            <div
+                className="sm:hidden group w-full rounded-lg bg-purple-500 p-4 transition relative duration-300 cursor-pointer hover:translate-y-[3px] hover:shadow-[0_-8px_0px_0px_#2196f3] mb-4"
+            >
+                <p className="text-white text-2xl font-bold">
+                    { props.name }
+                </p>
+                <div className="flex items-center space-x-2">
+                    <IconCalendarEvent className="text-white" />
+                    <p className="text-white text-sm">{ props.subName }</p>
+                </div>
+            </div>
+            <div className="grid grid-cols-2 sm:grid-cols-5 gap-4">
                 <div
-                    className="group w-full rounded-lg bg-purple-500 p-4 transition relative duration-300 cursor-pointer hover:translate-y-[3px] hover:shadow-[0_-8px_0px_0px_#2196f3]"
+                    className="hidden sm:block group w-full rounded-lg bg-purple-500 p-4 transition relative duration-300 cursor-pointer hover:translate-y-[3px] hover:shadow-[0_-8px_0px_0px_#2196f3]"
                 >
                     <p className="text-white text-2xl font-bold">
                         { props.name }
