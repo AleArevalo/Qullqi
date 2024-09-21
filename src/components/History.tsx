cc65cc0d7bdd2899a99a63b50e35b3bf8baa060f
Alexander Arévalo <alexander_arevalo@icloud.com>
2024-09-21 01:29:49 -0300

    Update History component: Added responsive margin for improved layout consistency on smaller screens

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -3,7 +3,7 @@ import CalendarMonth from "./CalendarMonth"
 
 const History = (props: PropsHistory) => {
     return (
-        <div className="flex flex-col justify-center space-y-4">
+        <div className="flex flex-col justify-center space-y-4 sm:mt-0 mt-6">
             <CalendarMonth yearSelected={ props.yearSelected } monthSelected={ props.monthSelected } handleChangeDate={ props.handleChangeDate } />
         </div>
     )   
