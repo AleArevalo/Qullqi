627851b2e19006038ef95fb53f2869329eac67b2
Alexander Arévalo <alexander_arevalo@icloud.com>
2024-09-21 17:26:34 -0300

    Update CalendarMonth component: Adjust button icons for better clarity in navigation between months on different screen sizes

--- src/components/CalendarMonth.tsx
+++ src/components/CalendarMonth.tsx
@@ -36,10 +36,10 @@ const Calendar = (props: PropsHistory) => {
 
     return (
         <div className="relative">
-            <div className="flex sm:flex-col flex-row-reverse justify-center items-center sm:space-y-4 p-4 sm:p-0 gap-8 sm:gap-0">
+            <div className="flex sm:flex-col flex-row justify-center items-center sm:space-y-4 p-4 sm:p-0 gap-8 sm:gap-0">
                 <button onClick={ () => handleChangeDate(props.yearSelected, props.monthSelected - 1) } disabled={ props.monthSelected === 0 }>
                     <IconChevronUp className={ `hidden sm:block ${ props.monthSelected === 0 ? 'text-gray-600' : 'dark:text-white hover:text-gray-400' }` } />
-                    <IconChevronRight className={ `block sm:hidden ${ props.monthSelected === 0 ? 'text-gray-600' : 'dark:text-white hover:text-gray-400' }` } />
+                    <IconChevronLeft className={ `block sm:hidden ${ props.monthSelected === 0 ? 'text-gray-600' : 'dark:text-white hover:text-gray-400' }` } />
                 </button>
                 <button
                     onClick={ toggleTooltip }
@@ -49,7 +49,7 @@ const Calendar = (props: PropsHistory) => {
                 </button>
                 <button onClick={ () => handleChangeDate(props.yearSelected, props.monthSelected + 1)} disabled={ props.monthSelected === 11 }>
                     <IconChevronDown className={ `hidden sm:block ${ props.monthSelected === 11 ? 'text-gray-600' : 'dark:text-white hover:text-gray-400' }` } />
-                    <IconChevronLeft className={ `block sm:hidden ${ props.monthSelected === 11 ? 'text-gray-600' : 'dark:text-white hover:text-gray-400' }` } />
+                    <IconChevronRight className={ `block sm:hidden ${ props.monthSelected === 11 ? 'text-gray-600' : 'dark:text-white hover:text-gray-400' }` } />
                 </button>
             </div>
 
